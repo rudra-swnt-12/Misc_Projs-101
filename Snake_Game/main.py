39f65351d04011ec610307794b4adbb94038124a
Rudra Sawant <rudra.vksawnt@gmail.com>
2024-09-21 20:26:12 +0530

    Snake Game now saves Highscores of users

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -36,27 +36,13 @@ while game_on:
 
     #Detect collision with Wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     #Detect collision with Tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_on = False
-            score.game_over()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
--- Snake_Game/score.py
+++ Snake_Game/score.py
@@ -8,6 +8,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.color("white")
@@ -15,13 +17,21 @@ class Score(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align= ALIGNMENT, font= FONT)
+        self.clear()
+        self.write(f"Score: {self.score} HIGH SCORE: {self.highscore}", align= ALIGNMENT, font= FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align= "center", font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align= "center", font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+        self.update_score()
